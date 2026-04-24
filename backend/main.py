79245b54a635d6d77189e6dae5e3f88b62df8865
unknown <nagaralprakash0@gmail.com>
2026-04-24 17:56:04 +0530

    24th Commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
