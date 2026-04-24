95b645649596533816bebbcaedbb928a3702f576
unknown <nagaralprakash0@gmail.com>
2026-04-24 17:36:45 +0530

    20th Commit

--- backend/main.py
+++ backend/main.py
@@ -21,24 +21,6 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(title=settings.app_name, lifespan=lifespan)
 
-from fastapi import Request
-
-@app.middleware("http")
-async def force_cors_middleware(request: Request, call_next):
-    print(f"DEBUG: Incoming {request.method} request to {request.url}")
-    # If it's a preflight OPTIONS request, we handle it immediately
-    if request.method == "OPTIONS":
-        from fastapi.responses import Response
-        response = Response()
-    else:
-        response = await call_next(request)
-        
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With"
-    return response
-
-# Standard middleware as backup
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
